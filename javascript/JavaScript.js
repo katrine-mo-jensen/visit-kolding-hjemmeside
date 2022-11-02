7562f4a1481bc46bc3f7148f8aefdba846c217e2
Mathiassren <110809330+Mathiassren@users.noreply.github.com>
2022-11-02 14:08:38 +0100

    New

---
+++ javascript/JavaScript.js
@@ -0,0 +1,18 @@
+
+    const navSlide = () => {
+    const burger = document.querySelector('.burger');
+    const nav1 = document.querySelector('#menu');
+    
+    alert("hej")
+
+}
+   
+    //Toggle nav
+        burger.addEventListener('click', () => {
+        nav.classList.toggle('active');
+        alert("ok");
+        navSlide();
+    });
+
+
+
